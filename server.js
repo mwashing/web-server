b1bc2a63dee3c859a93eb00da0eff239e22b101d
Michael Washington <mwashington@westmonroepartners.com>
2016-09-16 14:54:39 -0700

    refactored middleware.js

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var middleware = require('./middleware.js')
 
 // adding in middleware to support route level activity
-var middleware = {
-	requireAuthentication: function(req,res,next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req,res,next){
-		console.log('Request: '+req.method + ' '+ req.originalUrl + ' ' + new Date().toString());
-		next();
-	}
-};
+
 
 // add global middleware
 
